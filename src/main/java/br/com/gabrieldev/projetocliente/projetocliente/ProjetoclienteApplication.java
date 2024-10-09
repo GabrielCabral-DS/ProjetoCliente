f62515dcb4b3c30a24894c3fa49f5f56b09a2373
GabrielCabral-DS <cabral.jj@hotmail.com>
2024-10-09 12:29:11 -0300

    Feat: Inicializando projeto cliente

---
+++ src/main/java/br/com/gabrieldev/projetocliente/projetocliente/ProjetoclienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrieldev.projetocliente.projetocliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoclienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabrieldev/projetocliente/projetocliente/ProjetoclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrieldev.projetocliente.projetocliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
